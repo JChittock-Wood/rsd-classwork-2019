f9eb5d4f740708adcc6e65df9cf8dd9baa26474c
Jacob Chittock-Wood <zcapjfc@ucl.ac.uk>
2019-10-31 11:30:41 +0000

    fixed variable name for 2nd test

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -9,7 +9,9 @@ def test_given_input():
     
 def test_class_time():
     first = time_range("2019-10-31 10:00:00", "2019-10-31 13:00:00")
-    second = time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 60 0)
+    second = time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
     result = overlap_time(first, second)
-    expected = short
+    expected = second
     assert result == expected
+    
+## 
